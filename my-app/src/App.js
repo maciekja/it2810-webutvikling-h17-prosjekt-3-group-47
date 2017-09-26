78d6981313a52fa83375a5e116df5c3723625b33
Jørgen Borgersen <Burg@dhcp-10-22-36-182.wlan.ntnu.no>
2017-09-26 13:31:02 +0200

    Notes first draft

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,188 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+class Main extends React.Component {
+   constructor(props) {
+    super(props);
+    this.newNote = this.newNote.bind(this); //when save form
+    this.saveNote = this.saveNote.bind(this); //localStorage & this.state
+    this.removeNote = this.removeNote.bind(this);
+    this.updateNote = this.updateNote.bind(this); //whan clicked save-icon in a note
+
+    this.state = {notes:[]};
+  }
+
+  componentDidMount() {
+    let notes = localStorage.getItem("notes");
+      if (notes)
+         this.setState({notes: JSON.parse(notes)});
+  }
+
+  newNote(title, text) { //prepend new object
+    let notes = [{title: title, text: text}].concat(this.state.notes);
+    this.saveNote(notes);
+  }
+
+  saveNote(notes) {
+    localStorage.setItem('notes', JSON.stringify(notes));
+    this.setState({notes: notes});
+  }
+
+  removeNote(index) {
+    let notes = this.state.notes;
+    notes.splice(index, 1);
+    this.saveNote(notes);
+  }
+
+  updateNote(index, title, text) {
+    let notes = this.state.notes;
+    notes[index].title = title;
+    notes[index].text = text;
+    this.saveNote(notes);
+  }
+
+  render() { //Conditional rendering: Ternary operator shows saved notes from local storage. Or plain text
+
+  let notes = this.state.notes.map((obj, i) =>
+               <Note key={i} index={i} title={obj.title} text={obj.text} onUpdate={this.updateNote} onRemove={this.removeNote} />
+               );
+
+    return ( <div className="container-fluid">
+
+              <div className="row header">
+                <h1 className="col-2">Notes</h1>
+                <div className="col-1 offset-7 offset-md-9">
+                  <button  type="button" className="btn btn-warning" data-toggle="collapse" data-target="#form">+</button>
+                </div>
+              </div>
+
+              <Form onSend={this.newNote}/>
+
+              <div className="container-fluid">
+                  {this.state.notes.length > 0 ? notes : ""}
+              </div>
+          </div>
+    )
   }
 }
 
-export default App;
+class Form extends React.Component {
+  constructor(props) {
+    super(props);    //controlled component, so it needs so many handlers
+    this.changeTitle = this.changeTitle.bind(this);
+    this.changeText = this.changeText.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+
+    this.state = {title: '', text: ''};
+  }
+
+  changeTitle(e) {
+    this.setState({title: e.target.value});
+  }
+
+  changeText(e) {
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) { //here is a "Lift state up" on the parent component
+    this.props.onSend(this.state.title, this.state.text);
+    this.handleCancel(e); //reset form text and cancel reload of the page
+  }
+
+  handleCancel(e) {
+    this.setState({title: '', text: ''});
+    e.preventDefault();
+  }
+
+  render() {
+    return ( <form className="collapse" id="form">
+                  <div className="form-group">
+                     <label htmlFor="exampleInputEmail1">Title</label>
+                     <input type="text" value={this.state.title} onChange={this.changeTitle}
+                            className="form-control" placeholder="Enter title" />
+                  </div>
+
+                  <div className="form-group">
+                     <textarea  name="text" value={this.state.text} onChange={this.changeText}
+                                placeholder="Enter message" className="form-control" rows="4"/>
+                  </div>
+
+                  <button onClick={this.handleSubmit} className="btn">Save</button>
+                  <button onClick={this.handleCancel} className="btn">Cancel</button>
+              </form>  )
+  }
+}
+
+class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeTitle = this.changeTitle.bind(this);
+    this.changeText = this.changeText.bind(this);
+
+    this.edit = this.edit.bind(this); //to the parent
+    this.delete = this.delete.bind(this); //to the parent
+
+    this.state = {title: this.props.title, text: this.props.text, editing: false}; //by default render as text
+  }
+
+  edit() { //lift state up to the parent
+    this.props.onUpdate(this.props.index, this.state.title, this.state.text);
+    this.setState({editing: !this.state.editing});
+  }
+
+  delete() { //lift state up to the parent
+    this.props.onRemove(this.props.index);
+  }
+
+  changeTitle(e) {
+    this.setState({title: e.target.value});
+  }
+
+  changeText(e) {
+    this.setState({text: e.target.value});
+  }
+
+  renderNoteOrEdit() { //when clicks edit button pencil-icon toggle between input and div
+    if(this.state.editing) {
+      return (<div className="inner">
+                <div className="title">
+                     <span>Title</span>
+                     <button type="button" className="btn del" onClick={this.delete}><i className="fa fa-trash"></i></button>
+                     <button type="button" className="btn save" onClick={this.edit}><i className="fa fa-floppy-o"></i></button>
+                </div>
+
+                <div className="form-group">
+                    <input type="text" value={this.state.title} onChange={this.changeTitle} className="form-control" />
+                </div>
+                <div className="form-group">
+                    <textarea  name="text" value={this.state.text} onChange={this.changeText} className="form-control" rows="4"/>
+                </div>
+
+             </div>)
+    } else {
+       return (<div className="inner">
+                 <div className="title">
+                     <h2>{this.props.title}</h2>
+                     <button type="button" className="btn del" onClick={this.delete}><i className="fa fa-trash"></i></button>
+                     <button type="button" className="btn" onClick={this.edit}><i className="fa fa-pencil"></i></button>
+                 </div>
+                 <div className="text">
+                   <p>{this.props.text}</p>
+                 </div>
+               </div>)
+    }
+  }
+  render() { //render function based on value {this.state.editing}
+    return ( <div className="note col-sx-10 col-sm-6 col-md-4">
+                {this.renderNoteOrEdit()}
+             </div>
+    )
+  }
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
+
+export default Main;
